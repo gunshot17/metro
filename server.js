b17dbbd92bf78d0f38036a058531643255aa64e1
gunshot17 <64939279+gunshot17@users.noreply.github.com>
2020-08-24 14:29:58 +0900

    master

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/config.env" });
+
+const users = require("./routes/user");
+
+const app = express();
+
+app.use("/api/v1/user", user);
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5777;
+app.listen(PORT, console.log("서버 가동"));
